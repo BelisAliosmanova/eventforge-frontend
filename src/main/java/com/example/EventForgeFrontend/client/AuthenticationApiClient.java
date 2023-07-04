25f087ba065ff4cb3be93ae0ad83d0a52f03a833
Belis Aliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-07-04 17:16:58 +0300

    Update AuthenticationApiClient.java

--- src/main/java/com/example/EventForgeFrontend/client/AuthenticationApiClient.java
+++ src/main/java/com/example/EventForgeFrontend/client/AuthenticationApiClient.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
 
-@FeignClient(name = "authentication-api-client" ,url = "https://eventgorge-api.up.railway.app/auth")
+@FeignClient(name = "authentication-api-client" ,url = "https://eventgorge-api.up.railway.app")
 
 public interface AuthenticationApiClient {
 
